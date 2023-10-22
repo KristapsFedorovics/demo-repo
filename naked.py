e7109166632669062a46f5638e6dbe6bf05ac14f
kristaps <kristaps.fedorovics@va.lv>
2023-10-22 12:50:28 +0000

    removed api key from naked.py

--- naked.py
+++ naked.py
@@ -5,14 +5,24 @@ import time
 import yaml
 
 from datetime import datetime
+from configparser import ConfigParser
+
 print('Asteroid processing service')
 
 # Initiating and reading config values
 print('Loading configuration from file')
 
 #NASA API key and  URL 
-nasa_api_key = "pBHsfidaqtOPhjOogOXYrABcETVmdjESws0RQMWi"
-nasa_api_url = "https://api.nasa.gov/neo/"
+try:
+	config = ConfigParser()
+	config.read('config.ini')
+
+	nasa_api_key = config.get('nasa', 'api_key')
+	nasa_api_url = config.get('nasa', 'api_url')
+
+except:
+	logger.exception('')
+	print('DONE')
 
 # Getting todays date
 dt = datetime.now()
